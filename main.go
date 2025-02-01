11337a56aef41f4f5c91f4cb0ba63c6250264bfd
Shamya <shamyaharia@gmail.com>
2025-02-02 00:22:44 +0530

    Adding Error Handling and Email Warming-Up Logic
    Email Warming-Up Logic:
    AWS SES has a warming-up period, meaning new accounts can send only a few emails initially:
    1.	A new account can send only 5 emails initially.
    	2.	After 10 API calls, the limit increases to 20 emails per day.
    	3.	After 20 API calls, the limit increases to 50 emails per day.

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import(
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"time"
 )
 
 var emailCount=0
+var emailLimit=5
+var firstEmailTime time.Time
 
 func main(){
 	r:=gin.Default()
@@ -14,18 +17,31 @@ func main(){
 	})
 
 	r.POST("/send-email",func(c*gin.Context){
+		resetLimitIfNeeded()
+		if emailCount>=emailLimit{
+			c.JSON(http.StatusTooManyRequests,gin.H{"error":"Sending Limit Exceeeded. Wait for 24hours!!",
+			})
+		return
+		}
 		emailCount++
 		c.JSON(http.StatusOK,gin.H{"message":"mock-message-id-123",
 		})
 	})
 
 	r.POST("/send-raw-email",func(c*gin.Context){
+		resetLimitIfNeeded()
+		if emailCount>=emailLimit{
+			c.JSON(http.StatusTooManyRequests,gin.H{"error":"Sending Limit Exceeeded. Wait for 24hours!!",
+			})
+		return
+		}
 		emailCount++
 		c.JSON(http.StatusOK,gin.H{"message":"mock-raw-message-id-456",
 		})
 	})
 
 	r.GET("/get-send-quota",func(c*gin.Context){
+		resetLimitIfNeeded()
 		c.JSON(http.StatusOK,gin.H{
 			"max24HourSend":200,
 			"sentLast24Hours":emailCount,
@@ -34,6 +50,7 @@ func main(){
 	})
 
 	r.GET("/get-send-statistics",func(c*gin.Context){
+		resetLimitIfNeeded()
 		c.JSON(http.StatusOK,gin.H{
 			"DeliveryAttempts":emailCount,
 			"Bounces":0,
@@ -43,4 +60,23 @@ func main(){
 	})
 
 	r.Run(":8080")
+}
+
+func resetLimitIfNeeded() {
+	if firstEmailTime.IsZero(){
+		return
+	}
+	if time.Since(firstEmailTime)>24*time.Hour{
+		emailCount=0
+		if emailLimit == 5{
+			emailLimit=10
+		}
+		else if emailLimit == 10{
+			emailLimit=20
+		}
+		else if emailLimit == 20{
+			emailLimit=50
+		}
+		firstEmailTime=time.Now()
+	}
 }
